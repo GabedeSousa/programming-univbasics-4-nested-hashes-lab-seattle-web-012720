1c62d24a7bbf53480c4f42e5f990c26238840688
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2019-12-31 01:24:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,9 @@ def adding_matz
      }
      programmer_hash.merge(:yukihiro_matsumoto => {
    :known_for => "Ruby",
-    :languages => ["LISP", "C"]})
+    :languages => ["LISP", "C"]
+       
+     )}
 
 end
 
